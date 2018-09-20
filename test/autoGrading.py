9827456c57d760064b0114983fd1a9be113e2ce5
unknown <aa10402tw@gmail.com>
2018-09-21 00:40:27 +0800

    09/21

--- test/autoGrading.py
+++ test/autoGrading.py
@@ -2,8 +2,43 @@ import subprocess
 import glob
 import os
 
-student_list = []
+student_id_list = []
 hw_id_list = ['hw1']
+
+class Student(object):
+	def __init__(self, stu_id, hw_id_list):
+		super(Student, self).__init__()
+		self.stu_id = stu_id
+		self.hw_id_list = hw_id_list
+		self.init_hw_info()
+
+	def init_hw_info(self):
+
+		self.hw_info = {}
+		for hw_id in self.hw_id_list:
+			self.hw_info[hw_id] = {}
+
+			### set mapping from hw_id to cfile path
+			cfile = '%s_%s.c'%(self.stu_id, hw_id) if os.path.isfile(cfile) else None
+			self.hw_info[hw_id]['cfile'] = cfile
+
+			### set hw state(not processed yet, pass, fail)
+			self.hw_info[hw_id]['state'] = 'not processed yet'
+
+			### fail_info
+			self.hw_info[hw_id]['fail_info'] = ''
+
+	def __str__(self):
+		s = "%s\n" %(self.stu_id)
+		for hw_id in self.hw_id_list:
+			s += '-'*50
+			s += '%s\n\t'%(hw_id)
+			s += 'cfile:%s, state:%s' %(self.hw_info[hw_id]['cfile'], self.hw_info[hw_id]['state'])
+		return s
+
+	def runTestCase(self, hw_id, testCase, expected_output):
+		pass
+
 
 for c_file_path in glob.glob('./*.c'):
 	c_file = os.path.basename(c_file_path)
